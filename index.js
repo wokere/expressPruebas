20a35118530793ea5104b9529d62773f239eefaa
Loli <wokere@gmail.com>
2021-03-31 21:57:09 +0200

    app level middleware

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const routegenerator = require('./routergen')
 // rutas!
-app.use('/p1', routegenerator('mihandler'))
+function middle(req,res,next){
+    console.log('middleware')
+    next()
+}
+app.use('/p1', middle, routegenerator('mihandler'))
 app.use('/p2', routegenerator('miotrohandler'))
-app.use('/p3', routegenerator('nocompletehandler'))
+app.use('/p3', middle,routegenerator('nocompletehandler'))
 
 app.listen(8888, (error)=> {
     if (error) throw Error
